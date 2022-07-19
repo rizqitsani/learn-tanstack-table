39ac771ddf70e60aa1ed0b40ac62ceed470f70b5
rizqitsani <rizqitsani@gmail.com>
2022-07-19 16:35:05 +0700

    chore: add react-query and axios

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,35 @@
+import {
+  QueryClient,
+  QueryClientProvider,
+  QueryOptions,
+} from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { AppProps } from 'next/app';
 
 import '@/styles/globals.css';
 
+import axiosClient from '@/lib/axios';
+
+const defaultQueryFn = async ({ queryKey }: QueryOptions) => {
+  const { data } = await axiosClient.get(`${queryKey?.[0]}`);
+  return data;
+};
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      queryFn: defaultQueryFn,
+    },
+  },
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} position='bottom-right' />
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
 
 export default MyApp;
